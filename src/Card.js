9d0624ccbaf5b1e649bf5cf2621899db2d6b5da7
Devon <devon.m.salazar@gmail.com>
2023-03-29 21:19:50 -0600

    Add ability for Card components to render into Explorer

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function Card() {
+function Card(props) {
   return (
-    <div>Card</div>
+    <div className='card'>
+        <img id='coverart' src={props.singlereview.url}/>
+        <div className='cardtext'>
+            <h2 id='titleheader'>{props.singlereview.title} - {props.singlereview.rating}</h2>
+            <p className='cardtext'>Artist Name: {props.singlereview.artistname}</p>
+            <p className='cardtext'>Release Date: {props.singlereview.releasedate}</p>
+            <p className='cardtext'>Genre: {props.singlereview.genre}</p>
+            <p className='cardtext'>Subgenre: {props.singlereview.subgenre}</p>
+            <p className='cardtext'>Highlight: {props.singlereview.favoritesong}</p>
+            <p className='cardtext'>Low point: {props.singlereview.leastfavoritesong}</p>
+            <p className='cardtext'>Review: {props.singlereview.reviewbody}</p>
+            <p className='cardtext'>Author: {props.singlereview.reviewauthor}</p>
+        </div>
+    </div>
   )
 }
 
--- src/Explorer.js
+++ src/Explorer.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Card from './Card'
 
-function Explorer() {
+function Explorer({ initialReviews }) {
+
+    const updatedReviews = initialReviews.map((review) => {
+        return(
+            <Card 
+            singlereview={review}
+            key={review.id}
+            />
+        )
+    })
+
   return (
     <div>
         <h1 id='exploreheader'>Explore Reviews</h1>
-        <p id='mainexpcontainer'></p>
+        <span id='mainexpcontainer'>{updatedReviews}</span>
     </div>
   )
 }
