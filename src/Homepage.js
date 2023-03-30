8906ac267592b5677f34ee1d47aa571c50af48f9
Devon <devon.m.salazar@gmail.com>
2023-03-29 18:49:22 -0600

    Add header and subheader text to Homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+        <h1 id='homepageheader'>Record Review</h1>
+        <h3 id='subheader'>Track, Rate, and Review Your Favorite Albums!</h3>
+    </div>
   )
 }
 
