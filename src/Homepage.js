0b64568829e01599ff44a5d2bdb4f5cf39270fd2
Devon <devon.m.salazar@gmail.com>
2023-03-30 02:22:46 -0600

    Update header and subheader content and formatting

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,8 +3,9 @@ import React from 'react'
 function Homepage() {
   return (
     <div id="backgroundimg">
-        <h1 id='homepageheader'>Record Review</h1>
-        <h3 id='subheader'>Track, Rate, and Review Your Favorite Albums!</h3>
+        <h1 id="homepageHeader">Record Review</h1>
+        <h3 className="homepageSubheader" id='subheader'>Track, Rate, and Review Your Favorite Albums!</h3>
+        <h3 className="homepageSubheader">A React Project by Devon Salazar</h3>
     </div>
   )
 }
